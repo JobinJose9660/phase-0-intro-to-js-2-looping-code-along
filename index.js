ac527bf0c9e7b59323d26e7790c2b807981a50d1
Jobin Jose <jjobin9660@gmail.com>
2023-03-23 00:50:27 -0400

    compelete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names,value){
+ let a= []   
+ for(let i=0;i<names.length;i++){
+   a.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);}
+   return a;
+
+}
+writeCards(["Guadalupe", "ollie", "Ali"],"surprise")
+
+function countDown(a){
+    while(a>=0){
+        console.log(a--);
+    }
+}
+countDown(11);
